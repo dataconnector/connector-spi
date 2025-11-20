8516d1cf6c73a08cd09f0dccb8f6a5e59696f2ed
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-11-20 15:36:14 +0700

    Return StreamCancellable from DataStreamSource
    - Replace ConnectorResult return type with StreamCancellable
    - Update javadoc to reflect cancellable handle

--- src/main/java/io/github/dataconnector/spi/DataStreamSource.java
+++ src/main/java/io/github/dataconnector/spi/DataStreamSource.java
@@ -1,7 +1,7 @@
 package io.github.dataconnector.spi;
 
 import io.github.dataconnector.spi.model.ConnectorContext;
-import io.github.dataconnector.spi.model.ConnectorResult;
+import io.github.dataconnector.spi.stream.StreamCancellable;
 import io.github.dataconnector.spi.stream.StreamObserver;
 
 /**
@@ -14,8 +14,8 @@ public interface DataStreamSource extends DataConnector {
      * 
      * @param context  The context of the connector.
      * @param observer The observer of the stream.
-     * @return The result of the start stream operation.
+     * @return The cancellable of the start stream operation.
      * @throws Exception If an error occurs while starting the stream.
      */
-    ConnectorResult startStream(ConnectorContext context, StreamObserver observer) throws Exception;
+    StreamCancellable startStream(ConnectorContext context, StreamObserver observer) throws Exception;
 }
